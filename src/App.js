b9019fe7f02d6899b7ba1354eadf5958670bf110
Michael Frieze <michaelfrieze@icloud.com>
2018-11-15 19:53:21 -0500

    getting started with hooks in the app file - this will not run yet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// All components are going to be functional
+// Get rid of everything in this create-react-app generated file
+// Not import React with useState
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+// Now we are going to create an App function
+// This is our main component
+// useState gives us 2 variables
+// the first is the value of the state like this.state within a class
+// it also gives us a function to update the state like this.setState
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false
+    }
+  ]);
+
+  // Now we want a return
+  // in the todo-list we want to map through the to-do's in the state
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
+// Export this App
 export default App;
