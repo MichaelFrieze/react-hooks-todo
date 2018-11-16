f5bc574a193370199636fdb8e0e24803b2b7dab9
Michael Frieze <michaelfrieze@icloud.com>
2018-11-15 23:20:28 -0500

    figured out why I was having problems with page refresh after form submit

--- src/App.js
+++ src/App.js
@@ -5,32 +5,26 @@ function Todo({ todo, index }) {
   return <div className="todo">{todo.text}</div>;
 }
 
-// Now adding To-Do form
-// Going to create another functional component
-// Takes in one prop, the method to add a to-do
-// Make sure to use brackets because we are using destructoring
-// Going to use the useState hook here since this form is going to use state
-// value for the state and setValue for the method that updates the state
-// the input classname is for styling
-// for onChange we need to be able to handle the submit so that needs to be created for the setValue
-function TodoForm({ addTodo }) {
+function TodoForm({addTodo}) {
   const [value, setValue] = useState('');
-  console.log('What is value? - ', value);
-  console.log('What is setValue? - ', setValue);
 
-  // since it is a submit we want ot make sure we prevent a default action
-  // we don't want to submit an empty value
-  // also notice we clear the setValue
   const handleSubmit = e => {
+    // I missed this right here
+    e.preventDefault();
     if (!value) return;
     addTodo(value);
     setValue('');
   };
 
-  // we should make this look a little nicer by using a placeholder
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" className="input" value={value} placeholder="Add Todo..." onChange={e => setValue(e.target.value)} />
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)}
+      />
     </form>
   );
 }
@@ -51,20 +45,11 @@ function App() {
     }
   ]);
 
-  console.log('What is todos? - ', todos);
-  console.log('What is setValue? - ', setTodos);
-
-  // each new todo is updating the state
-  // so we need to take the array of todos
-  // can use spread to copy what is there and add in text
-  // now we can set the Todos basically updating the state
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
 
-  // Under the map we need to put our to-do form
-  // we need to create the addTodo function above this
   return (
     <div className="app">
       <div className="todo-list">
