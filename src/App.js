ec8f521dbfcea53f1a9a8f1e19626fc9663122eb
Michael Frieze <michaelfrieze@icloud.com>
2018-11-16 05:28:59 -0500

    add ability to remove to-do's

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Todo({ todo, index, completeTodo }) {
-  // lets add to this div a style
-  // remember style uses double curly bracers
-  // we also want to add a couple of buttons
-  // going to add an onClick to the button using an arrow func
-  // we also want to pass in completeTodo as a prop
-  // very similar to our addTodo
+// so we need to add a new button
+// lets call it removeTodoRef since it is going to get a reference rather than a value
+// Label the button as X
+// bring this in as a prop as well
+function Todo({ todo, index, completeTodoRef, removeTodoRef }) {
   return (
     <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} className="todo">
       {todo.text}
       <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => completeTodoRef(index)}>Complete</button>
+        <button onClick={() => removeTodoRef(index)}>x</button>
       </div>
     </div>
   );
 }
 
-function TodoForm({ addTodoReference }) {
+function TodoForm({ addTodoRef }) {
   const [value, setValue] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!value) return;
-    addTodoReference(value);
+    addTodoRef(value);
     setValue('');
   };
 
@@ -62,22 +61,31 @@ function App() {
     setTodos(newTodos);
   };
 
-  // now we need to create completeTodo
-  // remember we still need to add newTodos isCompleted true to our state
   const completeTodo = index => {
     const newTodos = [...todos];
     newTodos[index].isCompleted = true;
     setTodos(newTodos);
   };
 
-  // Here we are going to add completeTodo as a prop to the Todo component
+  // Now, lets create our deleteTodo function
+  // it needs an index
+  // now we are going to create a new array with spread
+  // then splice it out by the index and just 1
+  // then setTodos for the state
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  // Now add our removeTodoRef prop and assign it to removeTodo function we created above
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
+          <Todo key={index} index={index} todo={todo} completeTodoRef={completeTodo} removeTodoRef={removeTodo} />
         ))}
-        <TodoForm addTodoReference={addTodo} />
+        <TodoForm addTodoRef={addTodo} />
       </div>
     </div>
   );
