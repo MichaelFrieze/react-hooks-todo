c82cac4c432f6226e749b56bc870e59cfb62cbd0
Michael Frieze <michaelfrieze@icloud.com>
2018-12-06 08:54:54 -0500

    added build directory

---
+++ build/precache-manifest.feb3da62cb1ba6f07120c067b74a5bac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f754b78c8b9c3b518b",
+    "url": "/react-hooks-todo/static/js/runtime~main.b3f754b7.js"
+  },
+  {
+    "revision": "b0228920b100425bacb5",
+    "url": "/react-hooks-todo/static/js/main.b0228920.chunk.js"
+  },
+  {
+    "revision": "e5f1bd10652fc6b90e34",
+    "url": "/react-hooks-todo/static/js/1.e5f1bd10.chunk.js"
+  },
+  {
+    "revision": "b0228920b100425bacb5",
+    "url": "/react-hooks-todo/static/css/main.df2bb650.chunk.css"
+  },
+  {
+    "revision": "e933afdbd8a7fcaee87f656f1b93ee3e",
+    "url": "/react-hooks-todo/index.html"
+  }
+];
