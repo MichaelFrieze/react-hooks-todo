b4972dec33a92ad5b5229a7def10cc712f048917
Michael Frieze <michaelfrieze@icloud.com>
2018-12-06 08:50:53 -0500

    Updates

---
+++ precache-manifest.c863eb864baa596401eda74a8c9fa273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f754b78c8b9c3b518b",
+    "url": "/react-hooks-todo/static/js/runtime~main.b3f754b7.js"
+  },
+  {
+    "revision": "c72b0f8a54174ab6b1f8",
+    "url": "/react-hooks-todo/static/js/main.c72b0f8a.chunk.js"
+  },
+  {
+    "revision": "e5f1bd10652fc6b90e34",
+    "url": "/react-hooks-todo/static/js/1.e5f1bd10.chunk.js"
+  },
+  {
+    "revision": "c72b0f8a54174ab6b1f8",
+    "url": "/react-hooks-todo/static/css/main.077436d8.chunk.css"
+  },
+  {
+    "revision": "6c49d31475d66d58acd3cc669b54b03c",
+    "url": "/react-hooks-todo/index.html"
+  }
+];
